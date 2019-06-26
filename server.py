383ec02a08e5d6b1e2655e223c0f17e9becd5749
SkriiL <flx.grimm@gmail.com>
2019-06-26 19:38:59 +0200

    sid

--- server.py
+++ server.py
@@ -22,31 +22,31 @@ def connect():
 
 
 @sio.on('sign-in')
-def sign_in(str):
+def sign_in(str, sid):
     res = user.sign_in(str)
-    sio.emit('signed-in', res)
+    sio.emit('signed-in', res, room=sid)
 
 
 @sio.on('create-user')
-def create_user(user_str):
+def create_user(user_str, sid):
     errors = user.create(user_str)
     sio.emit('created-user', errors)
 
 
 @sio.on('get-all-users')
-def get_all_users(str):
+def get_all_users(str, sid):
     users = user.get_all()
     sio.emit('all-users', users)
 
 
 @sio.on('get-user-by-id')
-def get_user_by_id(user_id):
+def get_user_by_id(user_id, sid):
     u = user.get_single_by_id(user_id)
     sio.emit('single-user', u)
 
 
 @sio.on('verify-email')
-def verify_email(user_id):
+def verify_email(user_id, sid):
     user.verify_email(user_id)
 
 
@@ -54,13 +54,13 @@ def verify_email(user_id):
 
 
 @sio.on('get-friends-for-user')
-def get_friends_for_user(user_id):
+def get_friends_for_user(user_id, sid):
     f = friends.get_all_for_user(user_id)
     sio.emit('friends-for-user', f)
 
 
 @sio.on('delete-friend')
-def delete_friend(friend_id):
+def delete_friend(friend_id, sid):
     friends.delete(friend_id)
 
 
@@ -68,24 +68,24 @@ def delete_friend(friend_id):
 
 
 @sio.on('send-friend-request')
-def send_friend_request(users_str):
+def send_friend_request(users_str, sid):
     req = friend_request.send_request(users_str)
     sio.emit('new-friend-request', req)
 
 
 @sio.on('get-friend-requests-for-user')
-def get_friend_requests_for_user(user_id):
+def get_friend_requests_for_user(user_id, sid):
     reqs = friend_request.get_all_for_user(user_id)
     sio.emit('friend-requests-for-user', reqs)
 
 
 @sio.on('accept-friend-request')
-def accept_friend_request(fr_id):
+def accept_friend_request(fr_id, sid):
     friend_request.accept(fr_id)
 
 
 @sio.on('reject-friend-request')
-def reject_friend_request(fr_id):
+def reject_friend_request(fr_id, sid):
     friend_request.reject(fr_id)
 
 
