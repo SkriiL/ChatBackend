59ce4e4445ee903c25e5e2a719d30bd9288f98e9
SkriiL <flx.grimm@gmail.com>
2019-06-30 22:49:17 +0200

    #8 permissions

--- permissions.py
+++ permissions.py
@@ -2,7 +2,16 @@ import sqlite3
 
 
 def list_to_permission(perm_list):
-    return {'id': perm_list[0], 'name': perm_list[1]}
+    chat = perm_list[2] == 'True'
+    friends = perm_list[3] == 'True'
+    user = perm_list[4] == 'True'
+    return {
+        'id': perm_list[0],
+        'name': perm_list[1],
+        'chat': chat,
+        'friends': friends,
+        'user': user,
+    }
 
 
 def get_single_by_id(id):
